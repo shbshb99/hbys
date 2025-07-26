21838650e06b32c380b8015affbd70c590724923
shbshb99 <shbshb1212@gmail.com>
2025-07-26 11:47:22 +0900

    kakao button Del. local host url modify

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const WEDDING_INVITATION_URL = "http://localhost:8000/";
+export const WEDDING_INVITATION_URL = "https://hb-ys.netlify.app/";
 export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
